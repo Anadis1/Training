fb688092c513e2ee9ea54cde7d1fe5828828800d
Anadis <serafimdimaster@mail.ru>
2023-06-13 21:55:21 +0300

    lesson2

---
+++ Lessons/Lessons2/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿int what = new Random().Next(10, 99);
+
+Console.WriteLine($"Число - {what}");
+
+int n1 = what / 10;
+int n2 = what % 10;
+
+if(n1 > n2)
+{
+    Console.WriteLine($"Большее число - {n1}");
+}
+else
+{
+    Console.WriteLine($"Большее число - {n2}");
+}
