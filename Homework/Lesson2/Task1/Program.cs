d9034712a4211a5df0f6ec2e51458e918ce2a1ff
Anadis <serafimdimaster@mail.ru>
2023-06-14 12:05:04 +0300

    Homework2

---
+++ Homework/Lesson2/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число - ");
+string temporary = Console.ReadLine();
+int nunber = Convert.ToInt32(temporary);
+
+int index = 0;
+int nunber2 = 0;
+
+if(nunber > 99 && nunber < 1000)
+{
+    while(index <= 1)
+    {
+        nunber2 = nunber % 10;
+        nunber /= 10;
+        index += 1;
+    }
+    Console.Write($"Вторая цифра введенного числа - {nunber2}");
+}
+else
+{
+    Console.Write($"Это не трехзначное число");
+}
---
+++ Homework/Lesson2/Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.Write("Введите число - ");
+string temporary = Console.ReadLine();
+int number = Convert.ToInt32(temporary);
+
+int index = 0;
+int length = 0;                     // Количество цифр в числе
+int temporarynunber = number;
+int figure = 0;
+
+while(temporarynunber > 0)          // Определяем длинну числа
+{
+    temporarynunber /= 10;
+    length += 1;
+}
+
+if(length > 2)
+{
+    while(index < length - 2)
+    {
+        figure = number % 10;
+        number /= 10;
+        index += 1;
+    }
+    Console.Write(figure);
+}
+else
+{
+    Console.Write($"В числе отсутствует третья цифра");
+}
