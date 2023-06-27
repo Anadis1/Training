bca49c19ac58f98009c20362864c0ff1c87d8350
Anadis <serafimdimaster@mail.ru>
2023-06-28 02:10:19 +0300

    Homework6

---
+++ Homework/Lesson6/Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int numberint()                                 
+{
+    string numint = Console.ReadLine();
+    int numberint = Convert.ToInt32(numint);
+    return numberint;
+}
+
+int[] array1(int arg)   // Создаем массив на основе ввода пользователя
+{
+    int[] list = new int[arg];
+    for (int i = 0; i < arg; i++)
+    {
+        Console.Write($"Введите число № {i + 1} ---> ");
+        list[i] = numberint();
+    }
+    
+    for (int i = 0; i < arg; i++)    // Показываю массив в терминале
+    {
+        Console.Write($"{list[i]}\t");
+    }
+    
+    return list;
+}
+
+int posNum(int[] array)    // Считает кол-во положительных чисел
+{
+    int posnum1 = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            posnum1 += 1;
+        }
+    }
+    return posnum1;
+}
+
+Console.Write("Сколько чисел всего? ---> ");
+int num = numberint();
+int[] list1 = array1(num);
+int positiveNumbers = posNum(list1);
+
+Console.Write($"\nЧисел больше нуля ---> {positiveNumbers}");
+
---
+++ Homework/Lesson6/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+int numberint(string text)                                 // Запрашивает число у пользователя и переводит его в int
+{
+    int numberint = Convert.ToInt32(text);
+    return numberint;
+}
+
+Console.Write("Введите значение k1 ---> ");
+int k1 = numberint(Console.ReadLine());
+Console.Write("Введите значение b1 ---> ");
+int b1 = numberint(Console.ReadLine());
+Console.Write("Введите значение k2 ---> ");
+int k2 = numberint(Console.ReadLine());
+Console.Write("Введите значение b2 ---> ");
+int b2 = numberint(Console.ReadLine());
+
+int x = - (b1 - b2) / (k1 - k2);
+int y = k1 * x + b1;
+
+
+
+Console.Write($"\nТочка пересечения двух прямых ---> x-{x}, y-{y}");
