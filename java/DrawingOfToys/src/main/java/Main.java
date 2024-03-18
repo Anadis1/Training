7b98e14830a7861e6757f8d17cf2a4f056868f14
Anadis <serafimdimaster@mail.ru>
2024-03-18 22:25:38 +0300

    SQL1

--- java/DrawingOfToys/src/main/java/Main.java
+++ java/DrawingOfToys/src/main/java/Main.java
@@ -9,14 +9,5 @@ public class Main {
         toyVendingMachine.setTake(17);  // Загружаем игрушки в автомат
         System.out.println("Колличество игрушек в автомате - " + toyVendingMachine.ToysInTheVendingMachine());
         toyVendingMachine.theButtonForTheDrawing();     // Человек 1 нажимает на кнопку розыгрыша игрушек
-        toyVendingMachine.theButtonForTheDrawing();     // Человек 2 нажимает на кнопку розыгрыша игрушек
-        toyVendingMachine.theButtonForTheDrawing();     // Человек 3 нажимает на кнопку розыгрыша игрушек
-        toyVendingMachine.theButtonForTheDrawing();     // Человек 4 нажимает на кнопку розыгрыша игрушек
-        toyVendingMachine.theButtonForTheDrawing();     // Человек 5 нажимает на кнопку розыгрыша игрушек
-        toyVendingMachine.theButtonForTheDrawing();     // Человек 6 нажимает на кнопку розыгрыша игрушек
-        toyVendingMachine.theButtonForTheDrawing();     // Человек 7 нажимает на кнопку розыгрыша игрушек
-        toyVendingMachine.theButtonForTheDrawing();     // Человек 8 нажимает на кнопку розыгрыша игрушек
-        toyVendingMachine.theButtonForTheDrawing();     // Человек 9 нажимает на кнопку розыгрыша игрушек
-        toyVendingMachine.theButtonForTheDrawing();     // Человек 10 нажимает на кнопку розыгрыша игрушек
     }
 }
