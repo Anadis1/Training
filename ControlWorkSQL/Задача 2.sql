f13bcd3c575b6515c3ba53e146ca6c87d71befad
Anadis <serafimdimaster@mail.ru>
2024-03-18 23:20:28 +0300

    SQL2

---
+++ ControlWorkSQL/Задача 2.sql
@@ -0,0 +1,18 @@
+-- Выведите только четные числа от 1 до 10. Пример: 2,4,6,8,10
+
+DROP PROCEDURE IF EXISTS counter;
+DELIMITER $$
+CREATE PROCEDURE counter (n INT)
+BEGIN
+	DECLARE i VARCHAR(20) DEFAULT '';
+    DECLARE ind INT DEFAULT 0;
+    DECLARE num INT DEFAULT 0;
+	WHILE (ind < n - 1) DO
+		SET ind = ind + 2;
+		SET i = CONCAT(i, ' ', ind) ;
+	END WHILE;
+    SELECT i;
+END$$
+DELIMITER ;
+
+CALL counter(10);
